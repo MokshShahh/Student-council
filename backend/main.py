47d57a7a4977afc380a9376889c487e6b48a4e4b
Moksh <moksh13112006@gmail.com>
2026-03-15 23:50:30 +0530

    seperated models from backend

--- backend/main.py
+++ backend/main.py
@@ -2,41 +2,13 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 from datetime import datetime
 from typing import Annotated
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Session, SQLModel, create_engine, select
 import os
-from enum import Enum
+from models import *
 
 
 load_dotenv()
 
-class UserRole(str, Enum):
-    STUDENT = "student"
-    COMMITTEE = "committee"
-    ADMIN = "admin"
-
-class Committee(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(unique=True, index=True)
-    description: str | None = None
-
-class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    email: str = Field(unique=True, index=True)
-    hashed_password: str
-    role: UserRole = Field(default=UserRole.STUDENT)
-    committee_id: int | None = Field(default=None, foreign_key="committee.id")
-    is_verified: bool = Field(default=False)
-
-class Events(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    committee_id: int = Field(foreign_key="committee.id")
-    name: str = Field(index=True)
-    description: str | None = Field(default=None)
-    registration_link: str | None = None
-    banner: str | None = None
-    start_time: datetime | None = None
-    end_time: datetime | None = None
-    is_approved: bool = Field(default=False)
 
 postgresql_url = os.getenv("pg_url","env error")
 
